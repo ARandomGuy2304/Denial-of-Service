af7b0bfa4421635d6212ec2e796706aab00b760d
ARandomGuy2304 <izraylevmichael@gmail.com>
2025-12-23 18:55:13 +0100

    Add terminal-flood.sh for controlled DoS execution
    Script prompts user for confirmation before executing a DoS attack.

---
+++ terminal-flood.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "Do you really want to run this script (it will execute a real DoS attack, which can crash your system)? (y/n)" ans
+if [[ "$ans" == "y" || "$ans" == "Y" ]]; then
+sleep 1
+else
+echo "Operation canceled."
+exit
+fi
+while true; do
+gnome-terminal -- bash -c "while true; do gnome-terminal; done"
+done
